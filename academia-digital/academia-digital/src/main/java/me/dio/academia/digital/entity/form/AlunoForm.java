7ec3be273865f308f0c2c08f758f864c6ecf4d5c
GabrielSantoss <gabrazil03@hotmail.com>
2023-04-12 09:50:37 -0300

    realizando consultas avançadas

--- academia-digital/academia-digital/src/main/java/me/dio/academia/digital/entity/form/AlunoForm.java
+++ academia-digital/academia-digital/src/main/java/me/dio/academia/digital/entity/form/AlunoForm.java
@@ -3,17 +3,26 @@ package me.dio.academia.digital.entity.form;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class AlunoForm {
 
+  @NotBlank(message = "Prencha o campo corretamente")
+  @Size(min = 3, max = 50, message = "'${validatedValue}' precisa ser entre {min} e {max} caracteres.")
   private String nome;
 
+  @NotBlank(message = "Prencha o campo corretamente")
+  //@CPF(message = "'${validatedValue}' é invalido")
   private String cpf;
 
+  @NotBlank(message = "Prencha o campo corretamente")
+  @Size(min = 3, max = 50, message = "'${validatedValue}' precisa ser entre {min} e {max} caracteres.")
   private String bairro;
 
   private LocalDate dataDeNascimento;
